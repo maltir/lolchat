d64c4e31c5842a1f59f3f0314e8962e9cbe5b909
Seph1ra <mariecat23soccer@hotmail.com>
2014-06-28 21:05:29 -0400

    Update pour le mute essai 1
    La la la Ding ding ding

--- title.js
+++ title.js
@@ -4,20 +4,75 @@ var checkAttack = self.setInterval(function(){newAttack()},10000);
 var childCount=$("#chatmsgtable tbody").children().length;
 var count = 0;
 var lolchat = {};
+
+
 window.onclick=function(){
 	document.title=theTitle;
 	count=0;
 }
 window.onload=function(){
-	/*lolchat.color.putButton();
+  boutonAttaque();
+   
+   document.getElementById("btnMutePalais").onclick= function () {  if(document.getElementById("btnMutePalais").value  ==="sonP")
+																													  {
+																															
+																															document.getElementById("btnMutePalais").value = "muteP";
+																													  }
+																													  else
+																													  {
+																															document.getElementById("btnMutePalais").value = "sonP";
+
+																														}
+																													};
+   document.getElementById("btnMuteCombat").onclick=function () {  if(document.getElementById("btnMuteCombat").value === "sonC")
+																												  {
+																														document.getElementById("btnMuteCombat").value = "muteC";
+
+																												  }
+																												  else
+																												  {
+																														document.getElementById("btnMuteCombat").value = "sonC";
+
+																													} 
+																														};
+  
+/*	lolchat.color.putButton();
 	lolchat.bdm.open();
 	lolchat.bdm.createTable();
 	lolchat.bdm.temp();
 	lolchat.bdm.getByMembre('0',lolchat.bdm.loadColorMembre);
+	f
+	document.getElementById("editDefaultBtn").onclick=function(){lolchat.color.edit('0');};*/
 	
-	document.getElementById("editDefaultBtn").onclick=function(){lolchat.color.edit('0');};
-	*/
 }
+
+function boutonAttaque()
+{
+	var positionButton = $("#ah_mid h1");
+	
+	var  boutonMute1 = document.createElement("input");
+	var boutonMute2 = document.createElement("input");
+	var form = document.createElement("form");
+  	
+	  //Assign different attributes to the element.
+	form.setAttribute("style", "text-align:left;");
+	form.setAttribute("type", "post");
+	form.setAttribute("onsubmit", "return false;"); 
+	
+	boutonMute1.setAttribute("type", "button");
+	boutonMute1.setAttribute("value", "sonC");
+	boutonMute1.setAttribute("id", "btnMuteCombat");
+	
+	boutonMute2.setAttribute("type", "button");
+	boutonMute2.setAttribute("value", "sonP");
+	boutonMute2.setAttribute("id", "btnMutePalais");
+	
+	form.appendChild(boutonMute1);
+	form.appendChild(boutonMute2);
+	positionButton[0].appendChild(form);
+}
+
+
 
 document.getElementById("chatinput").addEventListener("keydown", function(e) {
     if (!e) { var e = window.event; }
@@ -39,9 +94,13 @@ function testNewMessageChat(){
 }
 
 function titleChange(){
+
 	document.title="*Nouveau message*";
-	var newMessageSound = new Audio('http://s1download-universal-soundbank.com/mp3/sounds/newemail.mp3');
-	newMessageSound.play();
+	if( document.getElementById("btnMutePalais").value  ==="muteP")
+	{
+		var newMessageSound = new Audio('http://s1download-universal-soundbank.com/mp3/sounds/newemail.mp3');
+		newMessageSound.play();
+	}
 }
 
 function titleChangeNumber(number){
@@ -49,7 +108,7 @@ function titleChangeNumber(number){
 }
 
 function newAttack(){
-	if($("#cbtbadge").attr("style")=="display: block;"){
+	if($("#cbtbadge").attr("style")=="display: block;" && document.getElementById("btnMuteCombat").value === "muteC"){
 		var newMessageSound = new Audio('http://s1download-universal-soundbank.com/mp3/sounds/4212.MP3');
 		newMessageSound.play();
 	}
